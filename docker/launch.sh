3c2a21d58284b21e46439bc3d34160dc541b1de8
karthik <karthiksuarbathula@gmail.com>
2020-06-17 13:47:08 -0400

    Updated files

--- docker/launch.sh
+++ docker/launch.sh
@@ -8,7 +8,7 @@ echo "START Running Jmeter on `date`"
 #add properties
 echo server.rmi.ssl.disable=true >> /opt/apache-jmeter-5.3/bin/user.properties
 
-#Install PluginManager and plugins
+#Install PluginManager and plugins  https://jmeter-plugins.org/wiki/PluginsManagerAutomated/
 java -cp /opt/apache-jmeter-5.3/lib/ext/jmeter-plugins-manager-1.4.jar org.jmeterplugins.repository.PluginManagerCMDInstaller
 PluginsManagerCMD.sh install jpgc-casutg,jpgc-dummy
 PluginsManagerCMD.sh status
--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -14,7 +14,7 @@ if ps -p `cat $pwd/pid` > /dev/null
 then
    echo "Jenkins is running PID: `cat $pwd/pid`"
 else
-   nohup java -jar jenkins.war > jenkins.log 2>&1 & echo $!>$pwd/pid
+   nohup java -Dhudson.model.DirectoryBrowserSupport.CSP="sandbox allow-scripts allow-popups allow-popups-to-escape-sandbox; style-src 'unsafe-inline' *;" -jar jenkins.war > jenkins.log 2>&1 & echo $!>$pwd/pid
    echo "Jenkins started with PID: `cat $pwd/pid`"
 fi
 
